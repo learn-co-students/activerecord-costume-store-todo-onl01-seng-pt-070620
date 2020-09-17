9b1aa19f200858a0debdddb166af1ac8b02fb754
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-17 17:15:05 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       create_table :costume_stores do |t|
         t.string :name
         t.string :location
-        t.integer :costume_invetory
+        t.integer :costume_inventory
         t.integer :num_of_employees
         t.boolean :still_in_business
         t.datetime :opening_time
