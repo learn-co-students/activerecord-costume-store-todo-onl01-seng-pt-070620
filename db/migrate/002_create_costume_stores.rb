ad3702f0ce120434e5a559211f1dd54b098530af
github username <github email address>
2020-09-18 09:55:00 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  
+    def change
+      create_table :costume_stores do |t|
+        t.string :name
+        end 
+    end 
+        
+end 
+  
